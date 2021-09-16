9b2654fcd9c90177620435f0bdfaa6357140ccd1
Noahkoole <Noahkoole@gmail.com>
2021-09-16 15:06:57 +0200

    Added navbar

--- Frontend/clientapp/src/layout/AppWrapper.tsx
+++ Frontend/clientapp/src/layout/AppWrapper.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from './Navbar/Navbar'
 
 interface AppWrapper_Children {
     children: React.ReactNode
@@ -7,6 +8,7 @@ interface AppWrapper_Children {
 export default function AppWrapper(props: AppWrapper_Children) {
     return (
         <React.Fragment>
+            <Navbar />
             {props.children}
         </React.Fragment>
     )
---
+++ Frontend/clientapp/src/layout/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import { useLocation, useHistory } from 'react-router';
+import { VscAccount } from 'react-icons/vsc';
+import { FiLogOut } from 'react-icons/fi';
+import "./NavbarStyles.scss";
+
+export default function Navbar() {
+
+    return (
+        <nav>
+            <NavbarRedirects />
+            <NavbarUserSection />
+        </nav>
+    )
+}
+
+function NavbarRedirects() {
+    const { pathname } = useLocation();
+    const history = useHistory();
+
+    console.log(pathname);
+ 
+    return (
+        <section>
+            <img src={require("../../assets/LOGO_Q3.png").default} />
+            <p className={pathname == "/monitoring" ? "bold-text" : ""} onClick={() => history.push("monitoring")}>Machine Monitoring</p>
+            <p className={pathname == "/chealth" ? "bold-text" : ""} onClick={() => history.push("chealth")}>Component Health</p>
+        </section>
+    )
+}
+
+function NavbarUserSection() {
+    return (
+        <section>
+            <div className="Name-Container">
+                <p>First name</p>
+                <p>Last name</p>
+            </div>
+            <VscAccount id="Profile-Icon" />
+            <FiLogOut id="Logout-Icon" />
+        </section>
+    )
+}
