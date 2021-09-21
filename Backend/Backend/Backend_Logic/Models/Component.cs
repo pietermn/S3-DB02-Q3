b898ef5426f2099ab7bb816a208fc72a9698bf15
GivanWiggers <436715@student.fontys.nl>
2021-09-21 15:22:30 +0200

    Component Model Made

--- Backend/Backend/Backend_Logic/Models/Component.cs
+++ Backend/Backend/Backend_Logic/Models/Component.cs
@@ -4,5 +4,23 @@ namespace Backend_Logic
 {
     public class Component
     {
+        public int Id { get;}
+        public string Name { get;}
+        public string Description { get;}
+        public int BuildYear { get;}
+        public int Port { get; }
+        public int Board { get; }
+        public int AmountOfUses { get; }
+
+        public Component(int id, string name, string description, int buildYear, int port, int board, int amountOfUses)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            BuildYear = buildYear;
+            Port = port;
+            Board = board;
+            AmountOfUses = amountOfUses;
+        }
     }
 }
