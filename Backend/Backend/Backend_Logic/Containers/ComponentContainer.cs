7ab5e0ca565220c2995ceb8db061190cf082e027
GivanWiggers <436715@student.fontys.nl>
2021-09-23 10:25:18 +0200

    First Container with interface

---
+++ Backend/Backend/Backend_Logic/Containers/ComponentContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Backend_Logic_Interface.Containers;
+using Backend_Logic_Interface.Models;
+
+namespace Backend_Logic.Containers
+{
+    public class ComponentContainer: IComponentContainer
+    {
+        public List<IComponent> GetComponents()
+        {
+            return ConvertToIComponentList();
+        }
+
+
+
+        private List<IComponent> ConvertToIComponentList()
+        {
+            return new List<IComponent>();
+        }
+    }
+}
---
+++ Backend/Backend/Backend_Logic_Interface/Containers/IComponentContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Backend_Logic_Interface.Models;
+
+namespace Backend_Logic_Interface.Containers
+{
+    public interface IComponentContainer
+    {
+        public List<IComponent> GetComponents();
+    }
+}
