ee68dc9e44150b128d2058b6896e920cf4fe93a4
Mullen,Pieter P.A.J. van der <github@vdmullen.nl>
2021-09-28 11:25:50 +0200

    Update q3_mms_dbContext.cs

--- Backend/Backend/Backend_DAL/q3_mms_dbContext.cs
+++ Backend/Backend/Backend_DAL/q3_mms_dbContext.cs
@@ -9,9 +9,6 @@ namespace Backend_DAL.DataAccess.DataObjects
 {
     public partial class q3_mms_dbContext : DbContext
     {
-        public q3_mms_dbContext()
-        {
-        }
 
         public q3_mms_dbContext(DbContextOptions<q3_mms_dbContext> options)
             : base(options)
@@ -25,14 +22,14 @@ namespace Backend_DAL.DataAccess.DataObjects
         public virtual DbSet<TellerstandenDTO> Tellerstandens { get; set; }
         public virtual DbSet<TreeviewDTO> Treeviews { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySQL("server=localhost;port=3307;user=root;password=root;database=q3_mms_db");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseMySQL("server=localhost;port=3307;user=root;password=root;database=q3_mms_db");
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
