79007cd9d88a7183c1544222b53052f5f00e895f
Noah Koole <Noahkoole@gmail.com>
2021-10-21 12:42:52 +0200

    Started on lifespan page

--- Frontend/clientapp/src/pages/lifespan/LifespanPage.tsx
+++ Frontend/clientapp/src/pages/lifespan/LifespanPage.tsx
@@ -1,13 +1,37 @@
+import ComponentsTable from '../../components/lifespan/componentstable/ComponentsTable'
+import Modal from 'react-modal';
 import './LifespanPage.scss'
-import ComponentsTable from '../../components/componenthealth/componentstable/ComponentsTable'
+import { Component } from '../../globalTypes';
+import { useState } from 'react';
 
 export default function LifespanPage() {
+
+    const [show, setShow] = useState(false)
+    const [selectedComponent, setSelectedComponent] = useState<Component>();
+
+    function handleSelectedComponent(component: Component) {
+        setSelectedComponent(component);
+        setShow(true);
+    }
+
     return (
-        <section className="Component-Overview">
+        <div className="Lifespan-page">
+            <Modal
+                className="Modal"
+                isOpen={show}
+                ariaHideApp={false}
+                onRequestClose={() => setShow(false)}
+                contentLabel={selectedComponent?.description}
+                shouldCloseOnOverlayClick={true}
+            >
+                <h1>Production line {selectedComponent?.description}</h1>
+                <h2>No components found</h2>
+                <button onClick={() => setShow(false)}>Close</button>
+            </Modal>
+            <h1>Components <i>Sort by total actions</i></h1>
             <div className="center-table">
-                <h1>Components <i>Sort by total actions</i></h1>
-                <ComponentsTable components={[]} SetComponent={() => {}}/>
+                <ComponentsTable setSelectedComponet={handleSelectedComponent} />
             </div>
-        </section>
+        </div>
     )
 }
