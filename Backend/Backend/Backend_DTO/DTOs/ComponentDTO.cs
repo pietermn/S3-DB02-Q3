5354d48192aeea253b81e6f3ef849668a280a529
Mullen,Pieter P.A.J. van der <github@vdmullen.nl>
2021-11-04 11:39:38 +0100

    Updated ComponentDTO with MaxActions 0 check

--- Backend/Backend/Backend_DTO/DTOs/ComponentDTO.cs
+++ Backend/Backend/Backend_DTO/DTOs/ComponentDTO.cs
@@ -28,6 +28,16 @@ namespace Backend_DTO.DTOs
         public int MaxActions { get; set; }
         public int CurrentActions { get; set; }
         [NotMapped]
-        public int PercentageMaintenance => Convert.ToInt32((double)CurrentActions / (double)MaxActions * 100);
+        public int PercentageMaintenance => CalculatePercentage();
+
+        private int CalculatePercentage()
+        {
+            if(MaxActions != 0)
+            {
+                return Convert.ToInt32((decimal)CurrentActions / (decimal)MaxActions * 100);
+            }
+            return 0;
+        }
     }
+
 }
