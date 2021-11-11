7c8d55e0c99eca83bdff76f140c14b4ba9644ed6
Noah Koole <Noahkoole@gmail.com>
2021-11-11 12:03:35 +0100

    Started on selected showing

--- Frontend/clientapp/src/components/componenthealth/componentstable/ComponentsTable.tsx
+++ Frontend/clientapp/src/components/componenthealth/componentstable/ComponentsTable.tsx
@@ -4,31 +4,37 @@ import "../HistoryTable/HistoryTable.scss";
 import "./ComponentsTable.scss";
 
 interface ITableProps {
-  components: Component[];
-  SetComponent: (component: Component) => void;
+    components: Component[];
+    selectedComponentId: number;
+    SetComponent: (component: Component) => void;
 }
 
 export default function ComponentsTable(props: ITableProps) {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div className="Components-Table">
-      <div className="thead">
-        <div className="row">
-          <p>{t("name.label")}</p>
-          <p>{t("totalactions.label")}</p>
-        </div>
-      </div>
-      <div className="tbody">
-        {props.components.map((component: Component, index: number) => {
-          return (
-            <div key={index} onClick={() => props.SetComponent(component)} className="row">
-              <p>{component.description}</p>
-              <p>{component.totalActions}</p>
+    return (
+        <div className="Components-Table">
+            <div className="thead">
+                <div className="row">
+                    <p>{t("name.label")}</p>
+                    <p>{t("totalactions.label")}</p>
+                </div>
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+            <div className="tbody">
+                {props.components.map((component: Component, index: number) => {
+                    return (
+                        <div
+                            key={index}
+                            onClick={() => props.SetComponent(component)}
+                            className={props.selectedComponentId == component.id ? "row selected" : "row"}
+                        >
+                            <p>{component.description}</p>
+                            <p>{component.totalActions}</p>
+                            {props.selectedComponentId == component.id && <div className="triangle" />}
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
