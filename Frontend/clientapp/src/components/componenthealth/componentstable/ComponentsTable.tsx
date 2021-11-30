ed03ae1d9eb2729a72b07348aa3db0cd5cf4461f
Bram <57172933+bramio124@users.noreply.github.com>
2021-11-30 10:04:45 +0100

    DataGrid scaling width

--- Frontend/clientapp/src/components/componenthealth/componentstable/ComponentsTable.tsx
+++ Frontend/clientapp/src/components/componenthealth/componentstable/ComponentsTable.tsx
@@ -3,7 +3,7 @@ import { Component } from "../../../globalTypes";
 import "../HistoryTable/HistoryTable.scss";
 import "./ComponentsTable.scss";
 import { DataGrid, GridColDef, GridSortModel } from "@mui/x-data-grid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface ITableProps {
     components: Component[];
@@ -19,17 +19,25 @@ export default function ComponentsTable(props: ITableProps) {
         },
     ]);
     const { t } = useTranslation();
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+    let dgWidth = (innerWidth / 3 - 96) * 0.8;
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setInnerWidth(window.innerWidth);
+        });
+    }, []);
 
     const cols: GridColDef[] = [
         {
             field: "description",
             headerName: t("name.label"),
-            width: 250,
+            width: dgWidth * 0.7,
         },
         {
             field: "totalActions",
             headerName: t("totalactions.label"),
-            width: 150,
+            width: dgWidth * 0.3,
             align: "right",
             headerAlign: "right",
         },
