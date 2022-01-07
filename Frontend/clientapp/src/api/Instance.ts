b4be0c01eebc74ada4b837987e8b6c942fc693a2
Noah Koole <Noahkoole@gmail.com>
2022-01-07 20:13:35 +0100

    Added caching to requests

--- Frontend/clientapp/src/api/Instance.ts
+++ Frontend/clientapp/src/api/Instance.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
+import { setupCache } from "axios-cache-adapter";
+
+const cache = setupCache({
+    maxAge: 5 * 60 * 1000,
+});
+
+console.log(cache.store);
 
 const instance = axios.create({
     baseURL: "http://localhost:5200/",
+    adapter: cache.adapter,
     // baseURL: "http://192.168.0.166:5200",
 });
 
